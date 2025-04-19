5a48a9e5063d5432f865fb477ce54e05726116a1
Pranaya Ghimire <pranayaghimire88083@gmail.com>
2025-04-19 10:42:27 +0545

    First Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,96 @@
-import Image from "next/image";
+"use client"
+import React, { useEffect, useMemo, useRef, useState } from 'react'
 
-export default function Home() {
+const page = () => {
+  
+  const [hitValue,setHitValue] = useState(0);
+  const [timer,setTimer] = useState(60);
+  const [scoreValue,setScoreValue] = useState(0);
+  const [isGameOver,setIsGameOver] = useState(false);
+  const timerRef=useRef<NodeJS.Timeout | number>(0);
+
+  // Generate random numbers once on mount
+  const randomNumbers = useMemo(()=>{
+    return Array.from({length:98},()=>Math.floor(Math.random()*10+1))
+  },[scoreValue]);
+
+  // Handle timer countdown
+ const handleTimer = () => {
+  timerRef.current = setInterval(()=>{
+    setTimer(prev=>{
+      if(prev===0){
+        clearInterval(timerRef.current);
+        setIsGameOver(true);
+        return 0;
+      }
+      return prev-1;
+    });
+  },1000);
+ }
+
+//  Handle bubble click
+  const handleScore = (e:React.MouseEvent,num:number) => {
+    if(num=== hitValue){
+      setScoreValue(prev=>prev+10);
+      // Generate new hit value
+      setHitValue(Math.floor(Math.random()*10+1));
+    }
+  }
+  // New Game
+  const newGame = () => {
+    setIsGameOver(false);
+    setScoreValue(0);
+    setTimer(60);
+    handleTimer();
+  }
+
+  // Initialize game
+  useEffect(()=>{
+    setHitValue(Math.floor(Math.random()*10+1));
+    handleTimer();
+
+    // Cleanup interval on unmount
+    return () => {
+      if (timerRef.current)
+          clearInterval(timerRef.current);
+    }
+  },[]);
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className='flex flex-col items-center justify-center bg-gradient-to-bl from-zinc-700 to-zinc-900 min-h-screen'>
+      <h1 className='text-3xl font-bold mb-5'>Bubble Game</h1>
+     
+      {isGameOver?(
+              <div className=' flex flex-col items-center space-y-5 '>
+              <h1 className='text-2xl font-bold'>Game Over! Final Score: {scoreValue}</h1>
+              <p className='text-xl'>Click on New Game Button to Play Again</p>
+              <button
+              onClick={newGame}
+              className='bg-violet-600 text-xl hover:bg-violet-700 p-2 rounded-md  cursor-pointer'>New Game</button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      ):(
+        <div className='flex flex-col items-center justify-center'>
+           <div className='space-x-10 text-xl mb-5'>
+      <span>{`Hit: ${hitValue}`}</span>
+      <span>{`Timer: ${timer}`}</span>
+      <span>{`Score: ${scoreValue}`}</span>
+      </div>
+      <div
+        className='flex  flex-wrap w-[80%] h-[80%] bg-white rounded-md'>
+              {randomNumbers.map((num,i)=>(
+                
+                <button 
+               
+                onClick={(e)=>handleScore(e,num)}
+                key={i+1} className={` bg-green-600 hover:bg-green-700 mx-3 my-3  cursor-pointer rounded-full w-15 h-15`}>{num}</button>
+              ))}
+  
+        </div>
+        </div>
+        
+      )}
     </div>
-  );
+  )
 }
+
+export default page
